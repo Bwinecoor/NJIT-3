e729fcbc9ea3778beaf2e52a3fec044bf5990d2d
Bwinecoor <123968562+Bwinecoor@users.noreply.github.com>
2024-05-20 11:45:34 -0400

    Add files via upload

--- site.js
+++ site.js
@@ -1,33 +1,4 @@
-/* SITE.JS: THIS FILE CONTAINS THE METHODS/FUNCTIONS AND VARIABLES CONTROLLING YOUR SITE
-//
-*/
-
-// /* NOTE: MOVIES.JSON CONTAINS A LIST OF MOVIES AND ACCOMPANYING METADATA
-// //
-// //    They are in the following format:
-
-// //    title (String): the name of the movie
-// //    iscore (Number): the IMDB score
-// //    rating (String): the movie's MPAA rating
-// //    released (Array): the release date. Note that the order of the array is:  YYYY, MM, DD
-// //    country (String): the country of production
-// //    posters (Array): an array of String values with the URL to movie posters (in your img/ directory)
-// //    imdb (String): the URL to the corresponding IMDB website
-// //    website (String): the URL to the corresponding official website
-// //    likes (Number): a fictitious number of user likes
-// //    dislikes (Number): a fictitious number of user dislikes
-// //    posterindex (Number): a counter to use with the "posters" array to keep track of the current displayed poster index
-// //
-// FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
-// */
-
-
-
-
-
 const vue_app = Vue.createApp({
-    // This automatically imports your movies.json file and puts it into
-    // the variable: movies
     created() {
         fetch('movies.json').then(response => response.json()).then(json => {
             this.movies = json;
@@ -35,7 +6,6 @@ const vue_app = Vue.createApp({
     },
     data() {
         return {
-            // This holds your movies.json data.
             movies: [],
             title: "IMDB + Brandon’s Top 8 Movies",
             owner: "Brandon",
@@ -43,8 +13,7 @@ const vue_app = Vue.createApp({
         }
     },
     methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-            getMonthText(dateArray) {
+        getMonthText(dateArray) {
             const [year, month, day] = dateArray;
             let monthName;
             switch (month) {
@@ -92,9 +61,14 @@ const vue_app = Vue.createApp({
         posterClick(index) {
             const movie = this.movies[index];
             movie.posterindex = (movie.posterindex + 1) % movie.posters.length;
+        },
+        incrementLikes(index) {
+            this.movies[index].likes++;
+        },
+        incrementDislikes(index) {
+            this.movies[index].dislikes++;
         }
     }
 });
 
-vue_app.mount("#vue_app")
-
+vue_app.mount("#vue_app");
